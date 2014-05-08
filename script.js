e925180b540f79872b004943f76c389c9d9fb12e
jrbsu <hello@jossu.co.uk>
2014-05-08 11:58:22 +0200

    Fixes to JS and some style changes

--- script.js
+++ script.js
@@ -2,33 +2,19 @@ $(document).ready(function() {
 	var random = 0;
 	var colourArray = [];
 	var colour;
-	for (var i=0;i<6;i++) {
-		random = Math.floor(Math.random() * (15 + 1)) + 0;
-		switch (random) {
-		case 10:
-    		random = "A";
-    		break;
-		case 11:
-    		random = "B";
-    		break;
-		case 12:
-    		random = "C";
-    		break;
-		case 13:
-    		random = "0";
-    		break;
-		case 14:
-    		random = "1";
-    		break;
-		case 15:
-    		random = "2";
-    		break;
-    	default:
-    		break;
-    	}
+	for (var i=0;i<3;i++) {
+		random = Math.floor(Math.random() * (160 + 1));
     	colourArray.push(random);
     }
-    colour = "#"+colourArray[0]+colourArray[1]+colourArray[2]+colourArray[3]+colourArray[4]+colourArray[5];
+    colour = "rgb("+colourArray[0]+","+colourArray[1]+","+colourArray[2]+")";
 	console.log(colour);
 	$('a').css("color", colour);
+	$('a').mouseenter(function() {
+   		$(this).css("color","white");
+		$(this).css("background", colour);
+	});
+	$('a').mouseout(function() {
+		$(this).css("color", colour);
+		$(this).css("background", "none");
+	});
 });
