bc6197a981a4e200ef5bf83f4d7980e4180868e5
jrbsu <hello@jossu.co.uk>
2014-06-24 00:24:11 +0100

    changes

--- script.js
+++ script.js
@@ -38,22 +38,22 @@ $(document).ready(function randomColour() {
 */
 
 $(document).ready(function namedColour() {
-	var colourArray = ["RGB(41, 164, 124)", "RGB(150, 27, 51)", "RGB(49, 78, 84)", "RGB(112, 84, 167)", "RGB(178, 30, 103)", "RGB(111, 120, 45)", "RGB(197, 133, 32)", "RGB(59, 30, 109)", "RGB(96, 120, 136)", "RGB(116, 121, 163)", "RGB(33, 145, 174)", "RGB(47, 68, 178)", "RGB(182, 71, 80)", "RGB(55, 164, 43)", "RGB(125, 96, 77)", "RGB(172, 64, 33)", "RGB(172, 32, 148)", "RGB(222, 20, 57)", "RGB(77, 20, 61)"];
-	var nameArray = ["jungle green", "vivid burgundy", "dark slate grey", "royal purple", "rich maroon", "olivetone", "geebung", "midnight blue", "hoki", "deluge", "pelorous", "violet blue", "rose vale", "forest green", "Roman coffee", "rock spray", "fandango", "crimson", "blackberry"];
+	var colourArray = ["RGB(41, 164, 124)", "RGB(150, 27, 51)", "RGB(49, 78, 84)", "RGB(112, 84, 167)", "RGB(178, 30, 103)", "RGB(111, 120, 45)", "RGB(197, 133, 32)", "RGB(59, 30, 109)", "RGB(96, 120, 136)", "RGB(116, 121, 163)", "RGB(33, 145, 174)", "RGB(47, 68, 178)", "RGB(182, 71, 80)", "RGB(55, 164, 43)", "RGB(125, 96, 77)", "RGB(172, 64, 33)", "RGB(172, 32, 148)", "RGB(222, 20, 57)", "RGB(77, 20, 61)", "RGB(199, 112, 8)"];
+	var nameArray = ["jungle green", "vivid burgundy", "dark slate grey", "royal purple", "rich maroon", "olivetone", "geebung", "midnight blue", "hoki", "deluge", "pelorous", "violet blue", "rose vale", "forest green", "Roman coffee", "rock spray", "fandango", "crimson", "blackberry", "chocolate"];
 	
 	var random = Math.floor(Math.random()*colourArray.length);
 	var colour = colourArray[random];
 	var colourName = nameArray[random];
     
 	$('h1').css("color", colour);
-	$('a').css("color", colour);
+	$('a').css("background", "#ccc");
 	$('a').mouseenter(function() {
    		$(this).css("color","white");
 		$(this).css("background", colour);
 	});
 	$('a').mouseout(function() {
-		$(this).css("color", colour);
-		$(this).css("background", "none");
+		$(this).css("color", "black");
+		$(this).css("background", "#ccc");
 	});
 	$('.explanation').css("border", "3px solid "+colour);
 	$('#colourinfo').html("You're viewing this page in <span style='color:"+colour+"; font-weight:600;'>"+colourName+"</span>.");
