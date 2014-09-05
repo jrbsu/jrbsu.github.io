5cc9e76fd595ffb23806672556b0e63e3986fc97
Joe Sutherland <hello@jossu.co.uk>
2014-09-05 08:16:37 +0100

    Fix image appearing on mobile

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 var colour;
 var screenWidth;
+var random;
+
+function windowResize() {
+	screenWidth = window.innerWidth;
+	console.log(screenWidth);
+	if (screenWidth > 900) {
+		$('body').css("background", "#DDDDDB url(img/bg"+random+".png) fixed no-repeat center right");
+	} else {
+		$('body').css("background", "#DDDDDB");
+	};
+};
 
 function namedColour() {
 	loadingBar();
 	var colourArray = ["RGB(41, 164, 124)", "RGB(150, 27, 51)", "RGB(49, 78, 84)", "RGB(112, 84, 167)", "RGB(178, 30, 103)", "RGB(111, 120, 45)", "RGB(197, 133, 32)", "RGB(59, 30, 109)", "RGB(96, 120, 136)", "RGB(116, 121, 163)", "RGB(33, 145, 174)", "RGB(47, 68, 178)", "RGB(182, 71, 80)", "RGB(55, 164, 43)", "RGB(125, 96, 77)", "RGB(172, 64, 33)", "RGB(172, 32, 148)", "RGB(222, 20, 57)", "RGB(77, 20, 61)", "RGB(199, 112, 8)", "RGB(248, 231, 16)"];
 	var nameArray = ["jungle green", "vivid burgundy", "dark slate grey", "royal purple", "rich maroon", "olivetone", "geebung", "midnight blue", "hoki", "deluge", "pelorous", "violet blue", "rose vale", "forest green", "Roman coffee", "rock spray", "fandango", "crimson", "blackberry", "chocolate", "lemon"];
 	
-	var random = Math.floor(Math.random()*colourArray.length);
+	random = Math.floor(Math.random()*colourArray.length);
 	console.log(random);
 	colour = colourArray[random];
 	var colourName = nameArray[random];
 	
-	$('body').css("background", "#DDDDDB url(img/bg"+random+".png) fixed no-repeat center right");
+	if (screenWidth > 1000) {
+		$('body').css("background", "#DDDDDB url(img/bg"+random+".png) fixed no-repeat center right");
+	} else {
+		$('body').css("background", "#DDDDDB!important");
+	};
     
 	$('h1').css("color", colour);
 	$('a').css("background", "#d4d4d4");
@@ -38,11 +53,6 @@ function loadingBar() {
     });
 };
 
-function windowResize() {
-	screenWidth = window.innerWidth;
-	console.log(screenWidth);
-};
-
 function slidersAndStars() {
 	windowResize();
 	if (screenWidth > 1000) {
@@ -64,12 +74,12 @@ function slidersAndStars() {
 };
 
 $(window).resize(function () {
-	slidersAndStars();
 	windowResize();
+	slidersAndStars();
 });
 
 $(document).ready(function () {
-	slidersAndStars();
 	windowResize();
+	slidersAndStars();
 	namedColour();
 });
