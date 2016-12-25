9da4bdf2113111c19dcf8bc4fa0d5d8363f10fe8
Joe Sutherland <1004421@rgu.ac.uk>
2016-12-25 18:14:34 +0000

    Total rehaul

--- script.js
+++ script.js
@@ -1,117 +1,97 @@
-var colour;
-var screenWidth;
-var random;
+var color, screenWidth, randomHue, borderColor, linkColor, titleColor, s;
 
-function windowResize() {
-	screenWidth = window.innerWidth;
-	if (screenWidth > 1000) {
-		$('body').css("background", "#DDDDDB url(img/bg"+random+".png) fixed no-repeat center right");
-	} else {
-		$('body').css("background", "#DDDDDB");
-	};
+// The following function is from https://codepen.io/pankajparashar/pen/oFzIg
+// Code by Pankaj Parashar
+function rgbToHsl(r, g, b, ll){
+	r /= 255, g /= 255, b /= 255;
+	var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	var h, s, l = (max + min) / 2;
+
+    if (max == min) { h = s = 0; } 
+    else {
+        var d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+        switch (max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return {
+        HSLCode: [(h*100+0.5)|0, ((s*100+0.5)|0) + '%'],
+        sat: s
+    };
 };
 
-function namedColour() {
-	loadingBar();
-	var colourArray = ["RGB(83, 153, 114)", "RGB(150, 27, 51)", "RGB(49, 78, 84)", "RGB(112, 84, 167)", "RGB(178, 30, 103)", "RGB(101, 124, 21)", "RGB(197, 133, 32)", "RGB(57, 40, 110)", "RGB(62, 82, 88)", "RGB(116, 121, 163)", "RGB(33, 145, 174)", "RGB(47, 68, 178)", "RGB(182, 71, 80)", "RGB(55, 164, 43)", "RGB(125, 96, 77)", "RGB(127, 59, 47)", "RGB(172, 32, 148)", "RGB(222, 20, 57)", "RGB(77, 20, 61)", "RGB(199, 112, 8)", "RGB(214, 206, 0)", ];
-	var nameArray = ["ocean green", "vivid burgundy", "dark slate grey", "royal purple", "rich maroon", "fern frond", "geebung orange", "midnight purple", "casal blue", "deluge", "pelorous", "violet blue", "rose vale", "forest green", "Roman coffee", "nutmeg", "fandango", "crimson", "blackberry", "mocha", "citrous"];
-	
-	random = Math.floor(Math.random()*colourArray.length);
-	console.log(random);
-	colour = colourArray[random];
-	var colourName = nameArray[random];
-	
-	if (screenWidth > 1000) {
-		$('body').css("background", "#DDDDDB url(img/bg"+random+".png) fixed no-repeat center right");
-	} else {
-		$('body').css("background", "#DDDDDB!important");
-	};
+function namedColor() {
+    // Testing:
+    colorArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
     
-	$('h1').css("color", colour);
-	$('a').css("background", "#d4d4d4");
-	$('a').mouseenter(function() {
-   		$(this).css("color","white");
-		$(this).css("background", colour);
-	});
-	$('a').mouseout(function() {
-		$(this).css("color", "#111");
-		$(this).css("background", "#d4d4d4");
-	});
-	$('ul').css("border-left", "3px solid "+colour);
-	$('.star').css("color", colour);
-	$('#colourinfo').html("You're viewing this page in <span style='color:"+colour+"; font-weight:600;'>"+colourName+"</span>.");
-	$('#slug').html("is a journalist and communicator, seen here in <span style='color:"+colour+"; font-weight:600;'>"+colourName+"</span>.");
-};
+	var randomRGBColor = ["#"];
+    
+    for (i=0; i<6; i++) {
+        randomNumber = Math.floor(Math.random() * 16);
+        randomRGBColor.push(colorArray[randomNumber]);
+    }
+    
+    randomRGBColor = randomRGBColor.join("");
 
-function loadingBar() {
-    new QueryLoader2(document.querySelector("body"), {
-        barColor: "#111",
-        backgroundColor: "#d4d4d4",
-        percentage: false,
-        barHeight: 1,
-        minimumTime: 50,
-        fadeOutTime: 200
-    });
+    function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+    function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+    function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+    function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
+    
+    R = hexToR(randomRGBColor);
+    G = hexToG(randomRGBColor);
+    B = hexToB(randomRGBColor);
+    
+	randomHue = Math.floor(Math.random() * 360);
+    textColor = "#111";
+    bgColor = "#ededed";
+    sat = rgbToHsl(R,G,B).sat >= 0.5 ? "#111" : "#fff";
+    newHSL = rgbToHsl(R,G,B).HSLCode;
+    
+    borderColor = "hsla(" + newHSL + ", 50%, 1)";
+	linkColor = "hsla(" + newHSL + ", 75%, 1)";
+    darkerColor = "hsla(" + newHSL + ", 35%, 1)";
+    lighterColor = "hsla(" + newHSL + ", 95%, 1)";
+    lighterColor2 = "hsla(" + newHSL + ", 85%, 1)";
+    titleColor = rgbToHsl(R,G,B).sat >= 0.5 ? "#111" : "#fff";
+    
+    $('body').css("background", "linear-gradient(" + lighterColor + "," + lighterColor2 + ")");
+	$('a').css("border-bottom", "2px solid " + linkColor);
+	$('a')
+        .mouseover(function () {
+            $(this).css("border-bottom", "none").css("border-bottom", "2px solid " + borderColor).css("color", darkerColor);
+        })
+        .mouseout(function () {
+            $(this).css("border-bottom", "2px solid " + linkColor).css("background", "none").css("color", textColor);
+        });
+//    $('h1').css("background", borderColor).css("color", titleColor);
+    $('h1').css("color", darkerColor).css("border-bottom", "20px solid " + darkerColor).css("border-left", "20px solid " + linkColor).css("border-top", "20px solid " + lighterColor2).css("border-right", "20px solid " + borderColor);
+    $('.h1-wrapper').css("color", borderColor);
+    $('.star')
+        .css("color", linkColor)
+        .mouseover(function () {
+            $(this).css("color", textColor);
+        })
+        .mouseout(function () {
+            $(this).css("color", linkColor);
+        });
+    $('.swatch1')
+        .css("background", bgColor);
+    $('.swatch2')
+        .css("background", borderColor);
+    $('.swatch3')
+        .css("background", linkColor);
 };
-
-function slidersAndStars() {
-	windowResize();
-	if (screenWidth > 1000) {
-		$("div.panels:eq(0)> ul.slide").hide();
-		$('div.panels:eq(0)> h3.panel').click(function() {
-			$(this).next('ul.slide:hidden').slideDown('fast')
-			.siblings('ul.slide:visible').slideUp('fast');
-			$('h3.panel').css("color", "#111");
-			$('.star').html("&#9734;&nbsp;");
-			$(this).css("color", colour);
-			$(this).children('.star').html("&#9733;&nbsp;");
-		//	$(this).next('span').html('&darr;');
-		//	$('div.panels:hidden').siblings('span').html('&rarr;');
-		});
-	} else {
-		$("div.panels:eq(0)> ul").show();
-		$('.star').html("&#9733;&nbsp;");
-	};
-};
-
-$(window).resize(function () {
-	windowResize();
-	slidersAndStars();
-});
 
 $(document).ready(function () {
-	windowResize();
-	slidersAndStars();
-	namedColour();
-	$('#alwayson').css("color", colour);
-	$("a[href^='http://']").attr("target","_blank");
-});
-
-// The following code is by Chris Coyier & tweaked by Mathias Bynens
-$(function() {
-	// Find all YouTube videos
-	var $allVideos = $("iframe[src^='https://www.youtube-nocookie.com']"),
-	    // The element that is fluid width
-	    $fluidEl = $(".panels");
-	// Figure out and save aspect ratio for each video
-	$allVideos.each(function() {
-		$(this)
-			.data('aspectRatio', this.height / this.width)
-			// and remove the hard coded width/height
-			.removeAttr('height')
-			.removeAttr('width');
-	});
-	// When the window is resized
-	// (You'll probably want to debounce this)
-	$(window).resize(function() {
-		var newWidth = $fluidEl.width();
-		// Resize all videos according to their own aspect ratio
-		$allVideos.each(function() {
-			var $el = $(this);
-			$el
-				.width(newWidth)
-				.height(newWidth * $el.data('aspectRatio'));
-		});
-	// Kick off one resize to fix all videos on page load
-	}).resize();
+	namedColor();
+    
+	$('#alwayson').css("color", color);
+	$("a[href^='http://']").attr("target", "_blank");
 });
